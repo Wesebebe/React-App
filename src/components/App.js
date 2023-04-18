ccb47895856a874b573a47b8bb04f72323a929e2
+ <+>
2023-04-18 08:49:46 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <img src="https://unsplash.com/photos/0kGDVRCd78Y" alt="nature" />
+      <img src="https://images.unsplash.com/photo-1680371130801-3701acb35d95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDc2fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="nature" />
       <p>"What is the good of your stars and trees, your sunrise and the wind, if they do not enter into our daily lives?" E.M. Forster</p>
     </div>
   )

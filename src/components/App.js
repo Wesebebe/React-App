e69915fabfd1e1cb29893b70775161fe822aaa9c
+ <+>
2023-04-18 08:43:25 +0300

    create react-app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <img src="https://unsplash.com/photos/0kGDVRCd78Y" alt="nature" />
+      <p>"What is the good of your stars and trees, your sunrise and the wind, if they do not enter into our daily lives?" E.M. Forster</p>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+// import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting () {
+    return <div>
+        <img src="https://images.unsplash.com/photo-1680371130801-3701acb35d95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDc2fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="nature" />
+        <p>"What is the good of your stars and trees, your sunrise and the wind, if they do not enter into our daily lives?" E.M. Forster</p>
+    </div>
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Greeting/>, document.getElementById('root'));
